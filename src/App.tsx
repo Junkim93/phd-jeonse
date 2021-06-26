e3f4e100effaab854968020337f9e82af0f70f54
TigerJquery <93kimhyunjun@gmail.com>
2021-06-26 16:46:06 +0900

    chore: 기본 파일 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import style from './App.module.scss'
+import cls from 'classnames'
+import Main from '@pages/main/index'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={cls(style.app)}>
+      <Main />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
