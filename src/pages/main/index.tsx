c7ee6a3de747f1098de3a5f2817a1c0e8b67fd0a
TigerJquery <93kimhyunjun@gmail.com>
2021-06-26 16:47:11 +0900

    New: 메인 페이지 추가

---
+++ src/pages/main/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styles from './index.module.scss'
+import cls from 'classnames'
+import MainHeader from '@components/main-header/index'
+import MainDescription from '@components/main-description/index'
+import Button from '@components/_reusable/button/index'
+import SideButton from '@components/_reusable/side-button/index'
+
+function Main() {
+  return (
+    <div className={cls(styles.main)}>
+      <MainHeader />
+      <div className={cls(styles.contents)}>
+        <MainDescription />
+        <div className={cls(styles.mainBtns)}>
+          <Button
+            label={'부동산 계약 전 십계명'}
+            type={'white'}
+            onClick={() => console.info('test')}
+          />
+          <Button
+            label={'체크리스트 보러가기'}
+            type={'red'}
+            onClick={() => console.info('test')}
+          />
+        </div>
+        <div className={cls(styles.sideBtns)}>
+          <SideButton
+            label={'제작자의 유언..'}
+            type={'gray'}
+            onClick={() => console.info('test')}
+          />
+          <SideButton
+            label={'디자이너는 귀엽'}
+            type={'white'}
+            onClick={() => console.info('test')}
+          />
+        </div>
+      </div>
+      <div className={cls(styles.bar)} />
+    </div>
+  )
+}
+
+export default Main
