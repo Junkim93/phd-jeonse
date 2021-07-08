72ce28395ed8453e5500edd237eaf88002e82b68
Junkim93 <93kimhyunjun@gmail.com>
2021-07-09 02:02:50 +0900

    Update: 버튼 컴포넌트 타입 및 스타일 정리

--- src/components/_reusable/button/index.tsx
+++ src/components/_reusable/button/index.tsx
@@ -7,17 +7,38 @@ import rightArrowWhite from '@assets/right-arrow-white.svg'
 interface ButtonProps {
   label: String
   onClick?: () => void
-  type?: 'white' | 'red'
+  type?: 'default' | 'route'
+  arrow?: boolean
+  theme?: 'white' | 'red'
+  className?: string
 }
 
-function Button({ label, onClick, type = 'white' }: ButtonProps) {
-  const arrow = type === 'white' ? rightArrowRed : rightArrowWhite
-  const buttonStyle = type === 'white' ? styles.white : styles.red
+function Button({
+  label,
+  onClick,
+  className,
+  type = 'default',
+  theme = 'red',
+  arrow = false,
+}: ButtonProps) {
+  const arrowSvg = theme === 'white' ? rightArrowRed : rightArrowWhite
+  const buttonTheme = theme === 'white' ? styles.white : styles.red
+  const buttonType =
+    type === 'default' ? styles.button__default : styles.button__route
 
   return (
-    <button className={cls(styles.button, buttonStyle)} onClick={onClick}>
+    <button
+      className={cls(styles.button, buttonType, buttonTheme, className)}
+      onClick={onClick}
+    >
       <span className={cls(styles.label)}>{label}</span>
-      <img className={cls(styles.arrow__img)} alt="오른쪽 화살표" src={arrow} />
+      {arrow ? (
+        <img
+          className={cls(styles.arrow__img)}
+          alt="오른쪽 화살표"
+          src={arrowSvg}
+        />
+      ) : null}
     </button>
   )
 }
